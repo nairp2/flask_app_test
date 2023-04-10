2ce58080a6243217b1cc97009a857d8e5adbaca2
Pranav Nair <nairp2@tcnj.edu>
2023-04-10 14:33:13 -0400

    Update

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Python Flask application is executed on jenkins pipeline\n"
+
+    result = "Python Flask application is executed on jenkins pipeline\n"
+    print(result)
+    return result
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000", threaded=False)
